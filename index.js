20777aeb096250400e9ebfee6706f3637637e0f6
MuhammadSarimWaseem <sarimwaseem84@gmail.com>
2025-02-04 19:44:43 +0500

    updated

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const router = require("./Routes/Multer");
-require('dotenv').config()
-const db = require("./config/db")
+const multerRoutes = require("./Routes/multer");
+const testRoutes = require("./Routes/test"); // Correctly import the second route
+require("dotenv").config();
+const db = require("./config/db");
 
 const app = express();
 const PORT = process.env.PORT;
 
-
-app.use(router)
-
+app.use(multerRoutes); // Prefix for clarity
+app.use(testRoutes); // Prefix for clarity
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
